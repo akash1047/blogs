532e7169d8ef81b1954a0219c846838c7a70c567
Akash Lohar <94453657+akash1047@users.noreply.github.com>
2024-09-12 19:08:19 +0530

    deployment testing
    * shadcn initialized
    
    * static adapter attached for static page genertion

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<main class="flex-1">
+	<slot />
+</main>
+
+<style>
+	:global(html) {
+		height: 100%;
+	}
+
+	:global(body) {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="flex flex-col items-center h-full justify-center">
+	<section class="p-6 rounded-2xl shadow-slate-300/90 shadow-xl">
+		<h1 class="text-2xl font-semibold">Hi I'm <span>Akash</span>.</h1>
+
+		<p>I'm a <span>software developer</span> from westbengal, india.</p>
+	</section>
+</div>
